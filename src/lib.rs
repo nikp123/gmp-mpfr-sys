72019b22c36a5b26f5506004845fa9606e449b99
Trevor Spiteri <trevor.spiteri@um.edu.mt>
2019-10-02 01:06:27 +0200

    allow clippy::missing_safety_doc

--- src/lib.rs
+++ src/lib.rs
@@ -245,7 +245,7 @@ To use a different directory, you can set the environment variable
 #![doc(test(attr(deny(warnings))))]
 #![cfg_attr(feature = "fail-on-warnings", deny(warnings))]
 #![cfg_attr(nightly_maybe_uninit, feature(maybe_uninit))]
-#![allow(clippy::identity_conversion)]
+#![allow(clippy::identity_conversion, clippy::missing_safety_doc)]
 
 pub mod gmp;
 #[cfg(feature = "mpc")]
